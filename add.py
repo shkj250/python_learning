b5ec8da4dd58d8282c0d0a46df8e6604957cf242
wuzhenyu <2198165392@qq.com>
2025-11-21 12:47:04 +0800

    优化Person类：支持初始化传参

--- add.py
+++ add.py
@@ -8,13 +8,13 @@ def abs(x):
     
 
 class Person:
-    def __init__(self):
-        self.name = "张三"
-        self.age = 20   
+    # 修改 __init__ 方法，增加 name 和 age 参数
+    def __init__(self, name="张三", age=20):
+        self.name = name
+        self.age = age   
     def info(self):
         return f"姓名：{self.name}，年龄：{self.age}"
     def set_name(self,name):
         self.name = name
     def set_age(self,age):
         self.age = age
-    
